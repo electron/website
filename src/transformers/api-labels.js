fb2fa3e952511aebbdafe59e8475a43a5f117726
Erick Zhao <erick@hotmail.ca>
2021-09-15 11:58:30 -0700

    docs: add description for api-labels transformer

--- src/transformers/api-labels.js
+++ src/transformers/api-labels.js
@@ -1,5 +1,27 @@
 //@ts-check
 
+/**
+ * This transformer adds badge styling to our raw API documentation.
+ * 
+ * Styles are added via the Badge component from Infima:
+ * https://infima.dev/docs/components/badge
+ * 
+ * The raw Markdown nodes to be transformed are:
+ * _macOS_
+ * _Linux_
+ * _Windows_
+ * _Readonly_
+ * _Deprecated_
+ * _Experimental_
+ * 
+ * This is done by modifying the node.data.hProperties of each
+ * matching AST node. the hProperties object is not directly documented
+ * on the MDX side, but is used in its transformation pipeline in the
+ * `mdast-util-to-hast` step.
+ * 
+ * See: https://github.com/syntax-tree/mdast-util-to-hast
+ */
+
 const visitParents = require("unist-util-visit-parents");
 
 module.exports = function attacher() {
