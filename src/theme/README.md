7e225b0e2874d3a1fd00bfd685e6f6653243b10b
Erick Zhao <erick@hotmail.ca>
2021-09-23 16:40:20 -0700

    feat: clean up API headings in TOC

---
+++ src/theme/README.md
@@ -0,0 +1,14 @@
+# Swizzled `src/theme` Components
+
+The contents of this folder are custom Docusaurus React components generated
+from the `yarn swizzle @docusaurus/theme-classic <component-name>` command.
+
+Note that Docusaurus discourages the use of this command during the beta phase
+due to API instability, so please keep swizzling to a minimum.
+
+> We discourage swizzling of components during the Docusaurus 2 beta phase.
+> The theme components APIs are likely to evolve and have breaking changes.
+> If possible, stick with the default appearance for now.
+
+For more reading, see the [Themes](https://docusaurus.io/docs/using-themes)
+guide in the Docusaurus documentation.
---
+++ src/theme/TOC/index.js
@@ -0,0 +1,27 @@
+
+/**
+ * WARNING: This is considered an 'unsafe' custom swizzle
+ * during the Docusaurus 2 beta phase. This component will
+ * override Docusaurus' own `@theme/TOC` export.
+ * 
+ * This is a relatively safe change because it re-uses the
+ * Docusaurus' underlying TOC and TOCHeadings components,
+ * but any change to the TOCItem type will affect our cleaning
+ * methods.
+ */
+
+//@ts-check
+import React from 'react';
+
+//@ts-expect-error
+import OriginalTOC, { TOCHeadings } from '@theme-original/TOC';
+import { cleanTOC } from '../../util/cleanHeadings';
+
+export { TOCHeadings };
+
+function TOC(props) {
+  const cleanedTOC = cleanTOC(props.toc);
+  return <OriginalTOC {...props} toc={cleanedTOC} />;
+}
+
+export default TOC;
