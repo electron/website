def99e9fc69dd36706e9e0be4a58f9100fd0f6f3
validity-check <mzaiddar77@gmail.com>
2021-10-26 22:44:35 +0100

    Initial page

---
+++ src/pages/community.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "@theme/Layout";
+
+function Community() {
+    return (
+        <Layout title="ElectronJS - Community">
+            Electron Community
+        </Layout>
+    )
+}
+
+export default Community;
