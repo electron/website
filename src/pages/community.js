bab2c8f7f2e6d207bee6790b50061e45a32b13bb
validity-check <mzaiddar77@gmail.com>
2021-10-26 23:22:17 +0100

    Add a subtron I don't think there's a need for a whole new style file just for the community section.

--- src/pages/community.js
+++ src/pages/community.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Layout from "@theme/Layout";
 
+import styles from './index.module.scss';
+import clsx from 'clsx';
+
 function Community() {
     return (
-        <Layout title="ElectronJS - Community">
-            Electron Community
+        <Layout title="Community">
+            <div className={clsx('subtron', styles.subtron)}>
+                <h1>Electron Community</h1>
+                <p>Resources for connecting with people working on Electron.</p>
+            </div>
         </Layout>
     )
 }
