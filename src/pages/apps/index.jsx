3b2aff75b78a0a1feb1c296a8211712a9b94ca15
Erick Zhao <erick@hotmail.ca>
2022-08-04 17:28:57 -0700

    zzz

---
+++ src/pages/apps/index.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Layout from '@theme/Layout';
+
+export default function AppsPage() {
+  return (
+    <Layout title="Governance">
+      <main className="container margin-vert--xl">
+        <h1>Hello World</h1>
+      </main>
+    </Layout>
+  );
+}
