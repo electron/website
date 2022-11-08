3c95962525e37d456a56418c68c13d1176df0e91
Erick Zhao <erick@hotmail.ca>
2022-11-08 15:24:17 -0800

    run prettier

--- src/theme/DocPage/Layout/index.js
+++ src/theme/DocPage/Layout/index.js
@@ -1,11 +1,11 @@
-import React, {useState} from 'react';
-import {useDocsSidebar} from '@docusaurus/theme-common/internal';
+import React, { useState } from 'react';
+import { useDocsSidebar } from '@docusaurus/theme-common/internal';
 import Layout from '@theme/Layout';
 import BackToTopButton from '@theme/BackToTopButton';
 import DocPageLayoutSidebar from '@theme/DocPage/Layout/Sidebar';
 import DocPageLayoutMain from '@theme/DocPage/Layout/Main';
 import styles from './styles.module.css';
-export default function DocPageLayout({children}) {
+export default function DocPageLayout({ children }) {
   const sidebar = useDocsSidebar();
   const [hiddenSidebarContainer, setHiddenSidebarContainer] = useState(false);
   return (
