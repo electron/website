7bf7db89bce16e4ee035f6f7f76f77c8b1a164d9
Erick Zhao <erick@hotmail.ca>
2022-11-24 11:08:55 -0800

    fix(i18n): remove docusaurus config hacks

--- scripts/i18n-build.js
+++ scripts/i18n-build.js
@@ -1,24 +1,8 @@
 //@ts-check
-const fs = require('fs').promises;
-const { join } = require('path');
 const { execute } = require('./utils/execute');
 const {
   i18n: { locales, defaultLocale },
 } = require('../docusaurus.config');
-
-const updateConfig = async (locale) => {
-  const baseUrl = locale !== defaultLocale ? `/${locale}/` : '/';
-  const configPath = join(__dirname, '../docusaurus.config.js');
-
-  let docusaurusConfig = await fs.readFile(configPath, 'utf-8');
-
-  docusaurusConfig = docusaurusConfig.replace(
-    /baseUrl: '.*?',/,
-    `baseUrl: '${baseUrl}',`
-  );
-
-  await fs.writeFile(configPath, docusaurusConfig, 'utf-8');
-};
 
 const processLocale = async (locale) => {
   const start = Date.now();
@@ -41,7 +25,6 @@ const start = async (locale) => {
 
   for (const locale of localesToBuild) {
     try {
-      await updateConfig(locale);
       await processLocale(locale);
     } catch (e) {
       // We catch instead of just stopping the process because we want to restore docusaurus.config.js
@@ -50,10 +33,6 @@ const start = async (locale) => {
       console.error(`Locale ${locale} failed. Please check the logs above.`);
     }
   }
-
-  // Restore `docusaurus.config.js` to the default values
-  await updateConfig(defaultLocale);
-
   console.log(`Process finished in ${(Date.now() - start) / 1000}s`);
 };
 
