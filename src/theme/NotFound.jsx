ad608299e5e83cbd255a434eac834dec4390c635
Erick Zhao <erick@hotmail.ca>
2023-11-02 15:16:01 -0700

    fixups

--- src/theme/NotFound.jsx
+++ src/theme/NotFound.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Translate, { translate } from '@docusaurus/Translate';
 import { PageMetadata } from '@docusaurus/theme-common';
-import errorimg from '../../static/assets/img/404.webp';
+import errorImage from '../../static/assets/img/404.webp';
 import './notFound.scss';
 import Layout from '@theme/Layout';
 export default function NotFound() {
@@ -17,25 +17,26 @@ export default function NotFound() {
         <main className="container margin-vert--xl">
           <div className="row">
             <div className="col">
-              <img src={errorimg} alt="computer image"></img>
+              <img src={errorImage} alt=""></img>
             </div>
-            <div className="col col--6 col--offset-2">
-              <h1 className="hero_title not_title">404</h1>
-              <h1 className="hero__title not_title2">
+            <div className="col col--7">
+              <h1 className="hero__title notFoundTitle">
                 <Translate
                   id="theme.NotFound.title"
                   description="The title of the 404 page"
                 >
-                  We could not find what you were looking for.
+                  Page Not Found
                 </Translate>
               </h1>
-              <p className="not_para">
+              <p>
                 <Translate
                   id="theme.NotFound.p1"
                   description="The first paragraph of the 404 page"
-                ></Translate>
+                >
+                  We could not find what you were looking for. Erick.
+                </Translate>
               </p>
-              <p className="not_para">
+              <p>
                 <Translate
                   id="theme.NotFound.p2"
                   description="The 2nd paragraph of the 404 page"
