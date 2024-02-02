c2cd66905244d85791185cc922d099afb7d12e92
David Sanders <dsanders11@ucsbalum.com>
2024-02-01 16:35:09 -0800

    fix: correct download link for Fiddle arm7l AppImage

--- src/pages/fiddle/index.tsx
+++ src/pages/fiddle/index.tsx
@@ -43,7 +43,7 @@ export default function FiddlePage() {
       appimage: {
         x64: `https://github.com/electron/fiddle/releases/download/v${version}/Electron.Fiddle-${version}-x64.AppImage`,
         arm64: `https://github.com/electron/fiddle/releases/download/v${version}/Electron.Fiddle-${version}-arm64.AppImage`,
-        arm7l: `https://github.com/electron/fiddle/releases/download/v${version}/Electron.Fiddle-${version}-armv7hl.AppImage`,
+        arm7l: `https://github.com/electron/fiddle/releases/download/v${version}/Electron.Fiddle-${version}-armv7l.AppImage`,
       },
     },
   };
