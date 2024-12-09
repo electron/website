98a3626188523e88b0e99e0fac6c4bb3eeb84bcd
Felix Rieseberg <fr@makenotion.com>
2024-12-09 14:30:19 -0800

    feat: Add a logo carousel (#676)
    * Add a logo carousel
    
    * Fix lint issues
    
    * fix: move helper outside of pages dir
    
    ---------
    
    Co-authored-by: Erick Zhao <erick@hotmail.ca>

---
+++ src/util/featured-apps.ts
@@ -0,0 +1,156 @@
+export interface FeaturedApp {
+  name: string;
+  image: string;
+  href: string;
+  isMonochrome?: boolean;
+}
+
+// Electron apps with "logo power"
+export const FEATURED_LOGOS: FeaturedApp[] = [
+  {
+    name: '1Password',
+    image: '/assets/app-logos/1password.svg',
+    href: 'https://1password.com/',
+  },
+  {
+    name: 'OpenAI ChatGPT',
+    image: '/assets/app-logos/openai.svg',
+    href: 'https://openai.com/',
+  },
+  {
+    name: 'Slack',
+    image: '/assets/app-logos/slack.svg',
+    href: 'https://slack.com/',
+  },
+  {
+    name: 'Anthropic Claude',
+    image: '/assets/app-logos/claude.svg',
+    href: 'https://claude.ai/',
+  },
+  {
+    name: 'Visual Studio Code',
+    image: '/assets/app-logos/vscode.svg',
+    href: 'https://code.visualstudio.com/',
+  },
+  {
+    name: 'Figma',
+    image: '/assets/app-logos/figma.svg',
+    href: 'https://figma.com/',
+  },
+];
+
+// Apps we _also_ love with less prominence
+export const FEATURED_APPS: FeaturedApp[] = [
+  {
+    name: '1Password',
+    image: '/assets/apps/1password.svg',
+    href: 'https://1password.com/',
+  },
+  {
+    name: 'Discord',
+    image: '/assets/apps/discord.svg',
+    href: 'https://discord.com/',
+  },
+  {
+    name: 'Dropbox',
+    image: '/assets/apps/dropbox.svg',
+    href: 'https://dropbox.com/',
+  },
+  {
+    name: 'Figma',
+    image: '/assets/apps/figma.svg',
+    href: 'https://figma.com/',
+  },
+  {
+    name: 'Loom',
+    image: '/assets/apps/loom.svg',
+    href: 'https://www.loom.com/',
+  },
+  {
+    name: 'Signal',
+    image: '/assets/apps/signal.svg',
+    href: 'https://signal.org/en/',
+  },
+  {
+    name: 'Skype',
+    image: '/assets/apps/skype.svg',
+    href: 'https://skype.com/',
+  },
+  {
+    name: 'Slack',
+    image: '/assets/apps/slack.svg',
+    href: 'https://slack.com/',
+  },
+  {
+    name: 'Notion',
+    image: '/assets/apps/notion.svg',
+    href: 'https://www.notion.so/',
+  },
+  {
+    name: 'VS Code',
+    image: '/assets/apps/vscode.svg',
+    href: 'https://code.visualstudio.com/',
+  },
+  {
+    name: 'Microsoft Teams',
+    image: '/assets/apps/teams.svg',
+    href: 'https://microsoft.com/en-ca/microsoft-teams/group-chat-software/',
+  },
+  {
+    name: 'Asana',
+    image: '/assets/apps/asana.svg',
+    href: 'https://asana.com/',
+  },
+  {
+    name: 'GitHub Desktop',
+    image: '/assets/apps/github-desktop.svg',
+    href: 'https://desktop.github.com/',
+  },
+  {
+    name: 'itch',
+    image: '/assets/apps/itchio.svg',
+    href: 'https://itch.io/app',
+  },
+  {
+    name: 'MongoDB Compass',
+    image: '/assets/apps/mongodb.svg',
+    href: 'https://www.mongodb.com/products/compass',
+  },
+  {
+    name: 'Obsidian',
+    image: '/assets/apps/obsidian.svg',
+    href: 'https://obsidian.md/',
+  },
+  {
+    name: 'Polypane',
+    image: '/assets/apps/polypane.svg',
+    href: 'https://polypane.app/',
+  },
+  {
+    name: 'Postman',
+    image: '/assets/apps/postman.svg',
+    href: 'https://postman.com/',
+  },
+  {
+    name: 'Splice',
+    image: '/assets/apps/splice.svg',
+    isMonochrome: true,
+    href: 'https://splice.com/',
+  },
+  {
+    name: 'Tidal',
+    image: '/assets/apps/tidal.svg',
+    href: 'https://tidal.com',
+    isMonochrome: true,
+  },
+  {
+    name: 'Trello',
+    image: '/assets/apps/trello.svg',
+    href: 'https://trello.com/',
+  },
+  {
+    name: 'Twitch',
+    image: '/assets/apps/twitch.svg',
+    href: 'https://www.twitch.tv/',
+  },
+];
